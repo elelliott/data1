2181a7f2a31a0dd919757db783f43f52075b387f
elelliott <elelliott@vassar.edu>
2014-09-17 21:30:49 -0400

    3 property check funcs; random tree func
    ALSO: just realized I have been committing but not syncing, oops!

--- FiniteSets/src/finitesets/FiniteSets.java
+++ FiniteSets/src/finitesets/FiniteSets.java
@@ -1,4 +1,5 @@
 package finitesets;
+import java.util.Random;
 
 public class FiniteSets {
 
@@ -11,33 +12,79 @@ public class FiniteSets {
     public static FiniteSet empty() {
         return new FSEmpty();
     }
+    
+    // a randomizer for number generation
+    static Random randomizer = new Random();
 
+    // Test Trees
+    static FiniteSet mt = empty();
+    static FiniteSet tree2 = empty().add(1).add(2);
+    static FiniteSet tree4 = empty().add(1).add(2).add(4).add(5);
+    
+    // randFS : no input --> fs
+    // generates a random finite set of random length (max. 100) and
+    // random entries (each element max. value 50)
+    public static FiniteSet randFS() {
+        FiniteSet tree = empty();
+        int numElts = randomizer.nextInt() % 100;
+        for (int i = 0; i < numElts; i++) {
+            tree.add(randomizer.nextInt() % 50);
+        }
+        return tree;
+    }
+    
+    
+    //////////////////////////// PROPERTIES ////////////////////////////
+    
     // for all x y s, x.union(y).subset(s) = x.subset(s) && y.subset(s)
+    public static void checkUnionSubset() {
+        FiniteSet x = randFS();
+        FiniteSet y = randFS();
+        FiniteSet s = randFS();
+        boolean answer = (x.union(y).subset(s) == (x.subset(s) && y.subset(s)));
+        System.out.println(answer + " should be " + true);
+    }
+    
     // for all x y, max(x.cardinality(), y.cardinality()) <= 
     //           x.union(y).cardinality() <= x.cardinality() + y.cardinality()
-    // for all t u x, (member inter(t u) x) = member t x && member u x
+    public static void checkUnionCard() {
+        FiniteSet x = randFS();
+        FiniteSet y = randFS();
+        int xlength = x.cardinality();
+        int ylength = y.cardinality();
+        boolean answer = ((Math.max(xlength, ylength) <= (x.union(y).cardinality()))
+                && (x.union(y).cardinality() <= (xlength + ylength)));
+        System.out.println(answer + " should be " + true);
+    }
+    
+    // for all t u x, t.inter(u).member(x) = t.member(x) && u.member(x)
+    public static void checkMemberInter() {
+        FiniteSet t = randFS();
+        FiniteSet u = randFS();
+        // x % 50 to mirror element value restriction in randFS
+        int x = randomizer.nextInt() % 50;
+        boolean answer = (t.inter(u).member(x) == (t.member(x) && u.member(x)));
+        System.out.println(answer + " should be " + true);
+    }
+    
+    
     // FOR ESSAY: Establish a standard, say why it's a good standard,
     //            then cite lines of code that illustrate the standard is met
-    static FiniteSet tree = empty();
-    static FiniteSet tree2 = empty().add(1).add(2);
-    static FiniteSet tree3 = empty().add(1).add(2).add(4).add(5);
 
-    public static int test() {
-        return tree.add(4).add(5).add(3).cardinality();
-    }
 
     public static void main(String[] args) {
-        System.out.println(test());
-        System.out.println(tree2.remove(1).cardinality());
-        System.out.println(tree2.remove(2).member(2));
-        System.out.println(tree2.union(tree3).member(4));
-        System.out.println(tree2.union(tree3).cardinality());
-        System.out.println(tree2.inter(tree3).member(2));
-        System.out.println(tree2.isEmptyHuh());
-        System.out.println(tree.isEmptyHuh());
-        System.out.println(tree2.diff(tree3).member(2));
-        System.out.println(tree.equal(tree2.remove(2).remove(1)));
-        System.out.println(tree2.subset(tree3));
+        System.out.println("Union & Subset property check:");
+        checkUnionSubset();
+        checkUnionSubset();
+        checkUnionSubset();
+        System.out.println("Union & Cardinality property check:");
+        checkUnionCard();
+        checkUnionCard();
+        checkUnionCard();
+        System.out.println("Member & Inter property check:");
+        checkMemberInter();
+        checkMemberInter();
+        checkMemberInter();
 
     }
 
