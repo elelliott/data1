3eee5788d68b9f18d30d7ddc60e3c9a20c966844
elelliott <elelliott@vassar.edu>
2014-09-18 10:48:06 -0400

    fixed randFS method
    no longer produces empty sets

--- FiniteSets/src/finitesets/FiniteSets.java
+++ FiniteSets/src/finitesets/FiniteSets.java
@@ -7,6 +7,8 @@ public class FiniteSets {
     ///////////////////////
     /////// TESTING ///////
     ///////////////////////
+    
+    
     // reiteration of empty method as bypass of static requirement
     // returns a fresh empty set
     public static FiniteSet empty() {
@@ -16,11 +18,6 @@ public class FiniteSets {
     // a randomizer for number generation
     static Random randomizer = new Random();
 
-    // Test Trees
-    static FiniteSet mt = empty();
-    static FiniteSet tree2 = empty().add(1).add(2);
-    static FiniteSet tree4 = empty().add(1).add(2).add(4).add(5);
-
     // randFS : no input --> fs
     // generates a random finite set of random length (max. 100) and
     // random entries (each element max. value 50)
@@ -28,59 +25,83 @@ public class FiniteSets {
         FiniteSet tree = empty();
         int numElts = randomizer.nextInt() % 100;
         for (int i = 0; i < numElts; i++) {
-            tree.add(randomizer.nextInt() % 50);
+            // add method ensures no duplicates arise
+            tree = tree.add(randomizer.nextInt() % 50);
         }
         return tree;
     }
 
     //////////////////////////// PROPERTIES ////////////////////////////
+    
     // for all x y s, x.union(y).subset(s) = x.subset(s) && y.subset(s)
     public static void checkUnionSubset() {
-        FiniteSet x = randFS();
-        FiniteSet y = randFS();
-        FiniteSet s = randFS();
-        boolean answer = (x.union(y).subset(s) == (x.subset(s) && y.subset(s)));
-        System.out.println(answer + " should be " + true);
+        for (int i = 0; i < 15; i++) {
+            FiniteSet x = randFS();
+            FiniteSet y = randFS();
+            FiniteSet s = randFS();
+            boolean answer = (x.union(y).subset(s) == (x.subset(s) && y.subset(s)));
+            System.out.println(answer + " should be " + true);
+        }
     }
 
     // for all x y, max(x.cardinality(), y.cardinality()) <= 
     //           x.union(y).cardinality() <= x.cardinality() + y.cardinality()
     public static void checkUnionCard() {
-        FiniteSet x = randFS();
-        FiniteSet y = randFS();
-        int xlength = x.cardinality();
-        int ylength = y.cardinality();
-        boolean answer = ((Math.max(xlength, ylength) <= (x.union(y).cardinality()))
-                && (x.union(y).cardinality() <= (xlength + ylength)));
-        System.out.println(answer + " should be " + true);
+        for (int i = 0; i < 15; i++) {
+            FiniteSet x = randFS();
+            FiniteSet y = randFS();
+            int xlength = x.cardinality();
+            int ylength = y.cardinality();
+            boolean answer = ((Math.max(xlength, ylength) <= (x.union(y).cardinality()))
+                    && (x.union(y).cardinality() <= (xlength + ylength)));
+            System.out.println(answer + " should be " + true);
+        }
     }
 
     // for all t u x, t.inter(u).member(x) = t.member(x) && u.member(x)
     public static void checkMemberInter() {
-        FiniteSet t = randFS();
-        FiniteSet u = randFS();
-        // x % 50 to mirror element value restriction in randFS
-        int x = randomizer.nextInt() % 50;
-        boolean answer = (t.inter(u).member(x) == (t.member(x) && u.member(x)));
-        System.out.println(answer + " should be " + true);
+        for (int i = 0; i < 15; i++) {
+            FiniteSet t = randFS();
+            FiniteSet u = randFS();
+            // x % 50 to mirror element value restriction in randFS
+            int x = randomizer.nextInt() % 50;
+            boolean answer = (t.inter(u).member(x) == (t.member(x) && u.member(x)));
+            System.out.println(answer + " should be " + true);
+        }
     }
 
-    // member (add t x) y = true <-> x = y \/ member t y = true
+    // for all t x y, t.add(x).member(y) = true <-> x = y \/ t.member(y) = true
     public static void checkMemberAdd() {
-        FiniteSet t = randFS();
-        int x = randomizer.nextInt() % 50;
-        int y = randomizer.nextInt() % 50;
-        boolean answer = (t.add(x).member(y) == ((x == y) || t.member(y)));
-        System.out.println(answer + " should be " + true);
+        for (int i = 0; i < 15; i++) {
+            FiniteSet t = randFS();
+            int x = randomizer.nextInt() % 50;
+            int y = randomizer.nextInt() % 50;
+            boolean answer = (t.add(x).member(y) == ((x == y) || t.member(y)));
+            System.out.println(answer + " should be " + true);
+        }
     }
 
-    // member (union s t) x = true <-> member s x = true \/ member t x = true
+    // t.union(s).member(x) = true <-> s.member(x) = true \/ t.member(x) = true
     public static void checkMemberUnion() {
-        FiniteSet s = randFS();
-        FiniteSet t = randFS();
-        int x = randomizer.nextInt() % 50;
-        boolean answer = (t.union(s).member(x) == (s.member(x) || t.member(x)));
-        System.out.println(answer + " should be " + true);
+        for (int i = 0; i < 15; i++) {
+            FiniteSet s = randFS();
+            FiniteSet t = randFS();
+            int x = randomizer.nextInt() % 50;
+            boolean answer = (t.union(s).member(x) == (s.member(x) || t.member(x)));
+            System.out.println(answer + " should be " + true);
+        }
+    }
+
+    // for all t u, t.subset(u) = true <-> t.equal(u) || 
+    //           t.diff(u).cardinality() == u.cardinality() - t.cardinality()
+    public static void checkSubEqualDiff() {
+        for (int i = 0; i < 15; i++) {
+            FiniteSet t = randFS();
+            FiniteSet u = randFS();
+            boolean answer = (t.subset(u) == (t.equal(u)
+                    || t.diff(u).cardinality() == u.cardinality() - t.cardinality()));
+            System.out.println(answer + " should be " + true);
+        }
     }
 
     // FOR ESSAY: Establish a standard, say why it's a good standard,
@@ -89,25 +110,16 @@ public class FiniteSets {
     public static void main(String[] args) {
         System.out.println("Union & Subset property check:");
         checkUnionSubset();
-        checkUnionSubset();
-        checkUnionSubset();
         System.out.println("Union & Cardinality property check:");
-        checkUnionCard();
-        checkUnionCard();
         checkUnionCard();
         System.out.println("Member & Inter property check:");
         checkMemberInter();
-        checkMemberInter();
-        checkMemberInter();
         System.out.println("Member & Add property check:");
-        checkMemberAdd();
-        checkMemberAdd();
         checkMemberAdd();
         System.out.println("Member & Union property check:");
         checkMemberUnion();
-        checkMemberUnion();
-        checkMemberUnion();
-
+        System.out.println("Subset, Equal, Diff/Cardinality property check:");
+        checkSubEqualDiff();
 
     }
 
